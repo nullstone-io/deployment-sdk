1144701191468bbbc6189b96a91cad57f9a1df7c
Brad Sickles <brad.sickles@gmail.com>
2022-07-26 14:23:56 -0400

    Refined ecs wait healthy logs

---
+++ aws/ecs/describe_tasks.go
@@ -0,0 +1,27 @@
+package ecs
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/nullstone-io/deployment-sdk/aws"
+)
+
+func DescribeTasks(ctx context.Context, infra Outputs, taskArns []string) ([]ecstypes.Task, error) {
+	if len(taskArns) == 0 {
+		return []ecstypes.Task{}, nil
+	}
+
+	ecsClient := ecs.NewFromConfig(nsaws.NewConfig(infra.Deployer, infra.Region))
+	out, err := ecsClient.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Cluster: aws.String(infra.Cluster.ClusterArn),
+		Tasks:   taskArns,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get task details: %w", err)
+	}
+
+	return out.Tasks, nil
+}
---
+++ aws/ecs/get_deployment_tasks.go
@@ -0,0 +1,43 @@
+package ecs
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/nullstone-io/deployment-sdk/aws"
+)
+
+func GetDeploymentTasks(ctx context.Context, infra Outputs, deploymentId string) ([]ecstypes.Task, error) {
+	ecsClient := ecs.NewFromConfig(nsaws.NewConfig(infra.Deployer, infra.Region))
+	out, err := ecsClient.ListTasks(ctx, &ecs.ListTasksInput{
+		Cluster:     aws.String(infra.Cluster.ClusterArn),
+		ServiceName: aws.String(infra.ServiceName),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get tasks associated with service (%s): %w", infra.ServiceName, err)
+	}
+
+	// if there aren't any tasks returned, we can't fetch any task descriptions
+	if len(out.TaskArns) == 0 {
+		return nil, nil
+	}
+
+	out2, err := ecsClient.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Cluster: aws.String(infra.Cluster.ClusterArn),
+		Tasks:   out.TaskArns,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get task details: %w", err)
+	}
+
+	tasks := make([]ecstypes.Task, 0)
+	for _, task := range out2.Tasks {
+		if task.StartedBy != nil && *task.StartedBy == deploymentId {
+			tasks = append(tasks, task)
+		}
+	}
+
+	return tasks, nil
+}
