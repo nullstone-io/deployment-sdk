e9836fc6e240258a1eee85e150517c251752ece8
Brad Sickles <brad.sickles@gmail.com>
2022-10-20 13:42:05 -0400

    Prevent thrashing of env vars

--- env-vars/standard.go
+++ env-vars/standard.go
@@ -9,10 +9,17 @@ func GetStandard(meta app.DeployMetadata) map[string]string {
 	}
 }
 
+// UpdateStandard returns an updated version of the env vars using new application deploy metadata
+// This does not add new env vars, only replaces existing ones
+// Otherwise, this could cause thrashing between code deploys and terraform plans
+// Essentially, we rely on the Terraform plan to be the source of truth for what env vars are included/excluded
 func UpdateStandard(existing map[string]string, meta app.DeployMetadata) map[string]string {
+	std := GetStandard(meta)
 	updated := existing
-	for k, v := range GetStandard(meta) {
-		updated[k] = v
+	for k := range existing {
+		if val, ok := std[k]; ok {
+			std[k] = val
+		}
 	}
 	return updated
 }
