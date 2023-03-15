c2aa4e97b3a31473e36bd3c6b5774eed6f08c11c
Brad Sickles <brad.sickles@gmail.com>
2023-03-15 09:00:41 -0400

    Implemented gcr pusher

---
+++ gcp/gcr/pusher.go
@@ -0,0 +1,117 @@
+package gcr
+
+import (
+	"context"
+	"fmt"
+	dockertypes "github.com/docker/docker/api/types"
+	"github.com/nullstone-io/deployment-sdk/app"
+	"github.com/nullstone-io/deployment-sdk/docker"
+	"github.com/nullstone-io/deployment-sdk/gcp"
+	"github.com/nullstone-io/deployment-sdk/logging"
+	"github.com/nullstone-io/deployment-sdk/outputs"
+	"gopkg.in/nullstone-io/go-api-client.v0"
+	"strings"
+)
+
+var (
+	GCRScopes = []string{"https://www.googleapis.com/auth/cloud-platform"}
+)
+
+type Outputs struct {
+	ImageRepoUrl docker.ImageUrl    `ns:"image_repo_url,optional"`
+	ImagePusher  gcp.ServiceAccount `ns:"image_pusher,optional"`
+}
+
+func NewPusher(osWriters logging.OsWriters, nsConfig api.Config, appDetails app.Details) (app.Pusher, error) {
+	outs, err := outputs.Retrieve[Outputs](nsConfig, appDetails.Workspace)
+	if err != nil {
+		return nil, err
+	}
+	return &Pusher{
+		OsWriters: osWriters,
+		Infra:     outs,
+	}, nil
+}
+
+type Pusher struct {
+	OsWriters logging.OsWriters
+	Infra     Outputs
+}
+
+func (p Pusher) Push(ctx context.Context, source, version string) error {
+	stdout, _ := p.OsWriters.Stdout(), p.OsWriters.Stderr()
+	// TODO: Log information to logger
+
+	sourceUrl := docker.ParseImageUrl(source)
+	targetUrl := p.Infra.ImageRepoUrl
+	targetUrl.Tag = version
+
+	if err := p.validate(targetUrl); err != nil {
+		return err
+	}
+
+	targetAuth, err := p.getGcrLoginAuth(ctx)
+	if err != nil {
+		return fmt.Errorf("error retrieving image registry credentials: %w", err)
+	}
+
+	dockerCli, err := docker.DiscoverDockerCli(p.OsWriters)
+	if err != nil {
+		return fmt.Errorf("error creating docker client: %w", err)
+	}
+
+	fmt.Fprintf(stdout, "Retagging %s => %s\n", sourceUrl.String(), targetUrl.String())
+	if err := dockerCli.Client().ImageTag(ctx, sourceUrl.String(), targetUrl.String()); err != nil {
+		return fmt.Errorf("error retagging image: %w", err)
+	}
+
+	fmt.Fprintf(stdout, "Pushing %s\n", targetUrl.String())
+	if err := docker.PushImage(ctx, dockerCli, targetUrl, targetAuth); err != nil {
+		return fmt.Errorf("error pushing image: %w", err)
+	}
+
+	return nil
+}
+
+func (p Pusher) validate(targetUrl docker.ImageUrl) error {
+	if targetUrl.String() == "" {
+		return fmt.Errorf("cannot push if 'image_repo_url' module output is missing")
+	}
+	if targetUrl.Tag == "" {
+		return fmt.Errorf("no version was specified, version is required to push image")
+	}
+	if !strings.Contains(targetUrl.Registry, "gcr.io") {
+		return fmt.Errorf("this app only supports push to GCP GCR (image=%s)", targetUrl)
+	}
+	// NOTE: For now, we are assuming that the production docker image is hosted in GCR
+	// This will likely need to be refactored to support pushing to other image registries
+	if p.Infra.ImagePusher.PrivateKey == "" {
+		return fmt.Errorf("cannot push without an authorized user, make sure 'image_pusher' output is not empty")
+	}
+
+	return nil
+}
+
+func (p Pusher) getGcrLoginAuth(ctx context.Context) (dockertypes.AuthConfig, error) {
+	ts, err := p.Infra.ImagePusher.TokenSource(ctx, GCRScopes...)
+	if err != nil {
+		return dockertypes.AuthConfig{}, fmt.Errorf("error creating access token source: %w", err)
+	}
+	token, err := ts.Token()
+	if err != nil {
+		return dockertypes.AuthConfig{}, fmt.Errorf("error retrieving access token: %w", err)
+	}
+	if token == nil || token.AccessToken == "" {
+		return dockertypes.AuthConfig{}, nil
+	}
+
+	serverAddr := p.Infra.ImageRepoUrl.Registry
+	if serverAddr == "" {
+		serverAddr = "gcr.io"
+	}
+	return dockertypes.AuthConfig{
+		ServerAddress: serverAddr,
+		Username:      "oauth2accesstoken",
+		Password:      token.AccessToken,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ecr v1.17.8
 	github.com/aws/aws-sdk-go-v2/service/ecs v1.18.11
 	github.com/aws/aws-sdk-go-v2/service/elasticbeanstalk v1.14.9
-	github.com/aws/aws-sdk-go-v2/service/iam v1.19.0
 	github.com/aws/aws-sdk-go-v2/service/lambda v1.23.4
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.1
 	github.com/aws/smithy-go v1.13.5
@@ -22,10 +21,15 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
 	github.com/vmihailenco/tagparser v0.1.2
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
 	gopkg.in/nullstone-io/go-api-client.v0 v0.0.0-20220921150731-89bf2bdaec43
+	k8s.io/api v0.20.6
+	k8s.io/apimachinery v0.20.6
+	k8s.io/client-go v0.20.6
 )
 
 require (
+	cloud.google.com/go v0.81.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.4.17 // indirect
 	github.com/Microsoft/hcsshim v0.9.0 // indirect
@@ -49,19 +53,25 @@ require (
 	github.com/docker/go-metrics v0.0.1 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/fvbommel/sortorder v1.0.2 // indirect
+	github.com/go-logr/logr v0.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.6.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/miekg/pkcs11 v1.0.3 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/moby/sys/mount v0.2.0 // indirect
 	github.com/moby/sys/mountinfo v0.4.1 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
@@ -83,6 +93,14 @@ require (
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/klog/v2 v2.4.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.0.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
