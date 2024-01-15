1215b9501038514a923fc4f23521a07023db4943
Brad Sickles <brad.sickles@gmail.com>
2024-01-15 12:55:40 -0500

    add metrics getter for beanstalk

--- block/aws-beanstalk/provider.go
+++ block/aws-beanstalk/provider.go
@@ -1,6 +1,7 @@
 package aws_beanstalk
 
 import (
+	"github.com/nullstone-io/deployment-sdk/aws/cloudwatch/metrics"
 	"github.com/nullstone-io/deployment-sdk/block"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 )
@@ -14,5 +15,5 @@ var ModuleContractName = types.ModuleContractName{
 }
 
 var Provider = block.Provider{
-	NewMetricsGetter: nil,
+	NewMetricsGetter: metrics.NewGetter,
 }
