d3d9fba4e11b0ee19e6b57dc0e012de1a5ef7126
Brad Sickles <brad.sickles@gmail.com>
2024-02-23 16:31:16 -0500

    Paging through results when listing docker remote tags

--- docker/list_remote_tags.go
+++ docker/list_remote_tags.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/tomnomnom/linkheader"
 	"io"
 	"net/http"
 	"net/url"
@@ -16,20 +17,38 @@ type ListRemoteTagsResponse struct {
 }
 
 func ListRemoteTags(ctx context.Context, targetUrl ImageUrl, targetAuth types.AuthConfig) ([]string, error) {
-	client := &http.Client{Transport: AuthedTransport{Auth: targetAuth}}
-	u := url.URL{
+	reqUrl := (&url.URL{
 		Scheme: targetUrl.Scheme(),
 		Host:   targetUrl.Registry,
 		Path:   fmt.Sprintf("/v2/%s/tags/list", targetUrl.RepoName()),
+	}).String()
+
+	allTags := make([]string, 0)
+	for {
+		pageTags, res, err := doListRemoteTags(ctx, reqUrl, targetAuth)
+		if err != nil {
+			return nil, err
+		} else if pageTags != nil {
+			allTags = append(allTags, pageTags...)
+		}
+
+		// Continue listing tags on the next page if the response contains a "next page"
+		if reqUrl = getNextPageUrl(res); reqUrl == "" {
+			return allTags, nil
+		}
 	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+}
+
+func doListRemoteTags(ctx context.Context, reqUrl string, targetAuth types.AuthConfig) ([]string, *http.Response, error) {
+	client := &http.Client{Transport: AuthedTransport{Auth: targetAuth}}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, reqUrl, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error creating list remote tags request: %w", err)
+		return nil, nil, fmt.Errorf("error creating list remote tags request: %w", err)
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error getting list remote tags response: %w", err)
+		return nil, nil, fmt.Errorf("error getting list remote tags response: %w", err)
 	}
 	if res.Body != nil {
 		defer res.Body.Close()
@@ -38,16 +57,29 @@ func ListRemoteTags(ctx context.Context, targetUrl ImageUrl, targetAuth types.Au
 	if res.StatusCode >= 400 {
 		raw, err := io.ReadAll(res.Body)
 		if err != nil {
-			return nil, fmt.Errorf("error reading error (status code = %d) response: %w", res.StatusCode, err)
+			return nil, res, fmt.Errorf("error reading error (status code = %d) response: %w", res.StatusCode, err)
 		}
-		return nil, fmt.Errorf("error response (status code = %d): %s", res.StatusCode, string(raw))
+		return nil, res, fmt.Errorf("error response (status code = %d): %s", res.StatusCode, string(raw))
 	}
 
 	decoder := json.NewDecoder(res.Body)
 	var result ListRemoteTagsResponse
 	if err := decoder.Decode(&result); err != nil {
-		return nil, fmt.Errorf("error reading json response: %w", err)
+		return nil, res, fmt.Errorf("error reading json response: %w", err)
 	}
 
-	return result.Tags, nil
+	return result.Tags, res, nil
+}
+
+func getNextPageUrl(res *http.Response) string {
+	val := res.Header.Get("Link")
+	if val == "" {
+		return ""
+	}
+	for _, link := range linkheader.Parse(val) {
+		if link.Rel == "next" {
+			return link.URL
+		}
+	}
+	return ""
 }
