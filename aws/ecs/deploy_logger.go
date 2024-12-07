84c9dc09adbed764f4cd1d10b26fe847c60d75d8
Brad Sickles <brad.sickles@gmail.com>
2024-12-07 12:35:53 -0500

    Added DeployStatusGetter.Close

--- aws/ecs/deploy_logger.go
+++ aws/ecs/deploy_logger.go
@@ -38,6 +38,8 @@ type DeployLogger struct {
 	lastSeenEventAt time.Time
 }
 
+func (d *DeployLogger) Close() {}
+
 func NewDeployLogger(ctx context.Context, osWriters logging.OsWriters, source outputs.RetrieverSource, appDetails app.Details) (app.DeployStatusGetter, error) {
 	outs, err := outputs.Retrieve[Outputs](ctx, source, appDetails.Workspace, appDetails.WorkspaceConfig)
 	if err != nil {
