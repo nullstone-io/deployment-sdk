032655e921798cb389a5676023280eea3c64513f
Brad Sickles <brad.sickles@gmail.com>
2024-12-18 13:39:50 -0500

    use new k8s deploy watcher

---
+++ aws/eks/deploy_watcher.go
@@ -0,0 +1,27 @@
+package eks
+
+import (
+	"context"
+	"github.com/nullstone-io/deployment-sdk/app"
+	"github.com/nullstone-io/deployment-sdk/k8s"
+	"github.com/nullstone-io/deployment-sdk/logging"
+	"github.com/nullstone-io/deployment-sdk/outputs"
+	"k8s.io/client-go/rest"
+)
+
+func NewDeployWatcher(ctx context.Context, osWriters logging.OsWriters, source outputs.RetrieverSource, appDetails app.Details) (app.DeployWatcher, error) {
+	outs, err := outputs.Retrieve[Outputs](ctx, source, appDetails.Workspace, appDetails.WorkspaceConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &k8s.DeployWatcher{
+		OsWriters:    osWriters,
+		Details:      appDetails,
+		AppNamespace: outs.ServiceNamespace,
+		AppName:      outs.ServiceName,
+		NewConfigFn: func(ctx context.Context) (*rest.Config, error) {
+			return CreateKubeConfig(ctx, outs.Region, outs.ClusterNamespace, outs.Deployer)
+		},
+	}, nil
+}
