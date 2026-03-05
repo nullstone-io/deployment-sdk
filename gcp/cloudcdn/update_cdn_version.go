ac42902f79a1ffd468ae15c8a712e3ab56d94fa8
Brad Sickles <brad.sickles@gmail.com>
2026-03-05 17:41:26 -0500

    Update gcp cdn paths with new version for route rules, default route and error policy

--- gcp/cloudcdn/update_cdn_version.go
+++ gcp/cloudcdn/update_cdn_version.go
@@ -1,15 +1,16 @@
 package cloudcdn
 
 import (
-	compute "cloud.google.com/go/compute/apiv1"
-	"cloud.google.com/go/compute/apiv1/computepb"
 	"context"
 	"fmt"
+	"strings"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/google/uuid"
 	"github.com/mitchellh/colorstring"
 	"github.com/nullstone-io/deployment-sdk/logging"
 	"google.golang.org/api/option"
-	"strings"
 )
 
 // UpdateCdnVersion updates the cloudfront distribution with the appropriate app version
@@ -30,9 +31,8 @@ func UpdateCdnVersion(ctx context.Context, infra Outputs, version string) (bool,
 		return false, err
 	}
 	hasChanges := false
-	newPathPrefix := coerceValidPathPrefix(infra.ArtifactsKey(version))
 	for _, urlMap := range urlMaps {
-		changed := updateUrlMapPathPrefix(ctx, urlMap, newPathPrefix)
+		changed := updateUrlMapPathPrefix(ctx, urlMap, version)
 		if !changed {
 			// We don't update the distribution if there were no changes or we don't support making changes
 			continue
@@ -54,34 +54,71 @@ func UpdateCdnVersion(ctx context.Context, infra Outputs, version string) (bool,
 	return hasChanges, err
 }
 
-func coerceValidPathPrefix(artifactsDir string) string {
-	if artifactsDir == "" {
-		return ""
-	}
-	// Ensure there is a single preceding `/`
-	if !strings.HasPrefix(artifactsDir, "/") {
-		artifactsDir = "/" + artifactsDir
-	}
-	// Drop trailing '/'
-	artifactsDir = strings.TrimSuffix(artifactsDir, "/")
-	return artifactsDir
-}
-
-func updateUrlMapPathPrefix(ctx context.Context, urlMap *computepb.UrlMap, newPathPrefix string) bool {
+func updateUrlMapPathPrefix(ctx context.Context, urlMap *computepb.UrlMap, newVersion string) bool {
 	stdout := logging.OsWritersFromContext(ctx).Stdout()
 
 	for _, pathMatcher := range urlMap.PathMatchers {
+		oldVersion := updateNullstoneVersionHeader(pathMatcher, newVersion)
+		if oldVersion == "" {
+			// We only update if we found X-Nullstone-Version header in this path matcher
+			continue
+		}
+
+		colorstring.Fprintf(stdout, "Updating path matcher %q\n", pathMatcher.GetName())
+		for i, routeRule := range pathMatcher.RouteRules {
+			if routeRule.RouteAction != nil && routeRule.RouteAction.UrlRewrite != nil {
+				ur := routeRule.RouteAction.UrlRewrite
+				ur.PathPrefixRewrite = replaceVersion(routeRule.RouteAction.UrlRewrite.PathPrefixRewrite, oldVersion, newVersion)
+				if ur.PathPrefixRewrite != nil {
+					colorstring.Fprintf(stdout, "Updated route_rules[%d].route_action.url_rewrite.path_prefix_rewrite with %q\n", i, *ur.PathPrefixRewrite)
+				}
+			}
+		}
 		if pathMatcher.DefaultRouteAction != nil && pathMatcher.DefaultRouteAction.UrlRewrite != nil {
 			ur := pathMatcher.DefaultRouteAction.UrlRewrite
-			existing := ur.PathPrefixRewrite
-			if existing != nil && *existing == newPathPrefix {
-				colorstring.Fprintf(stdout, "Path rule in url map (%s) is already configured with the correct app version\n", *urlMap.Name)
-				return false
+			ur.PathPrefixRewrite = replaceVersion(pathMatcher.DefaultRouteAction.UrlRewrite.PathPrefixRewrite, oldVersion, newVersion)
+			if ur.PathPrefixRewrite != nil {
+				colorstring.Fprintf(stdout, "Updated default_route_action.url_rewrite.path_prefix_rewrite with %q\n", *ur.PathPrefixRewrite)
 			}
-			ur.PathPrefixRewrite = &newPathPrefix
 			return true
 		}
+		if pathMatcher.DefaultCustomErrorResponsePolicy != nil {
+			for i, errorPolicy := range pathMatcher.DefaultCustomErrorResponsePolicy.ErrorResponseRules {
+				errorPolicy.Path = replaceVersion(errorPolicy.Path, oldVersion, newVersion)
+				if errorPolicy.Path != nil {
+					colorstring.Fprintf(stdout, "Updated default_custom_error_response_policy.error_response_rule[%d].path with %q\n", i, *errorPolicy.Path)
+				}
+			}
+		}
 	}
-	colorstring.Fprintf(stdout, "Could not find valid path rule in url map (%s) to update app version\n", *urlMap.Name)
 	return false
+}
+
+const (
+	appVersionHeaderName = "X-Nullstone-Version"
+)
+
+func updateNullstoneVersionHeader(matcher *computepb.PathMatcher, newVersion string) string {
+	if matcher.HeaderAction == nil {
+		return ""
+	}
+	for _, cur := range matcher.HeaderAction.RequestHeadersToAdd {
+		if cur.GetHeaderName() == appVersionHeaderName {
+			result := cur.GetHeaderValue()
+			cur.HeaderValue = &newVersion
+			return result
+		}
+	}
+	return ""
+}
+
+func replaceVersion(pathValue *string, oldVersion, newVersion string) *string {
+	if pathValue == nil {
+		return nil
+	}
+	if *pathValue == "" || oldVersion == "" || newVersion == "" {
+		return pathValue
+	}
+	updated := strings.ReplaceAll(*pathValue, oldVersion, newVersion)
+	return &updated
 }
