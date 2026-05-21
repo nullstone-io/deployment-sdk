9a295238963afe02b6b7e5ddc69b4d88dac7509f
Brad Sickles <brad.sickles@gmail.com>
2026-05-21 11:28:31 -0400

    Implemented cloudrun actioner

--- workspace/all/actioners.go
+++ workspace/all/actioners.go
@@ -4,9 +4,11 @@ import (
 	aws_ecs_ec2_provider "github.com/nullstone-io/deployment-sdk/app/container/aws-ecs-ec2"
 	aws_ecs_fargate_provider "github.com/nullstone-io/deployment-sdk/app/container/aws-ecs-fargate"
 	aws_eks_provider "github.com/nullstone-io/deployment-sdk/app/container/aws-eks"
+	gcp_cloudrun_provider "github.com/nullstone-io/deployment-sdk/app/container/gcp-cloudrun"
 	gcp_gke_service "github.com/nullstone-io/deployment-sdk/app/container/gcp-gke-service"
 	"github.com/nullstone-io/deployment-sdk/aws/ecs"
 	"github.com/nullstone-io/deployment-sdk/aws/eks"
+	"github.com/nullstone-io/deployment-sdk/gcp/cloudrun"
 	"github.com/nullstone-io/deployment-sdk/gcp/gke"
 	"github.com/nullstone-io/deployment-sdk/workspace"
 )
@@ -17,6 +19,7 @@ var (
 	Actioners = workspace.Actioners{
 		aws_eks_provider.ModuleContractName:         eks.NewActioner,
 		gcp_gke_service.ModuleContractName:          gke.NewActioner,
+		gcp_cloudrun_provider.ModuleContractName:    cloudrun.NewActioner,
 		aws_ecs_fargate_provider.ModuleContractName: ecs.NewActioner,
 		aws_ecs_ec2_provider.ModuleContractName:     ecs.NewActioner,
 	}
